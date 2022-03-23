653db739158335216b96a7211910dc4b179408fa
Жас <zhasulan@bk.ru>
2022-03-23 10:54:50 +0600

    cod style

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-//Первая задача:    
-    var button = document.querySelector('button');
+'use strict';
+//Первая задача:
+    const product = prompt('Наименование товара');
+    const amount = +prompt('Количество товара');
+    const category = prompt('Категория товара');
+    const price = prompt('Цена товара');
 
-    button.onclick = function() {
-        const product = prompt('Наименование товара');
-        const amount = +prompt('Количество товара');
-        const category = prompt('Категория товара');
-        const price = prompt('Цена товара');
-
-        if (amount > 0) 
-            {
-            console.log('На складе ' + amount + 
-            ' единиц товара ' + '\"' + product + '\"' + 
-            ' на сумму ' + amount * price + ' рублей'); 
-            } 
-        else 
-            {
-            alert('Вы ввели некорректные данные');   
-            }
-    }  
+    if (amount > 0) {
+        console.log('На складе ' + amount + ' единиц товара ' + '\"' + product + '\"' + ' на сумму ' + 
+        amount * price + ' рублей'); 
+    } else {
+        alert('Вы ввели некорректные данные');   
+    };  
     
--- tasksHard_1.js
+++ tasksHard_1.js
@@ -1,18 +1,9 @@
 //Дополнительная первая задача:    
-var button = document.querySelector('button');
-
-button.onclick = function() {
-    const earnings = +prompt('Укажите ваш доход: ');
-    if (earnings < 15000) 
-        {
+    const earnings = prompt('Укажите ваш доход: ');
+    if (+earnings < 15000) {
         console.log('13% на доход до 15 000 ₽ налог = ', Math.round(earnings/100*13));
-        }
-    else if (earnings >= 15000 && earnings < 50000) 
-        {
+    } else if (+earnings >= 15000 && +earnings < 50000) {
         console.log('20% на доход от 15 000 ₽ до 50 000 ₽ налог = ', Math.round(earnings/100*20));
-        }
-    else if (earnings >= 50000) 
-        {
+    } else if (+earnings >= 50000) {
         console.log('30% на доход выше 50 000 ₽ налог = ', Math.round(earnings/100*30));
-        }
-} 
+    };
